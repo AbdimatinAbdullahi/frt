50fd2ab9862e74a3c428d774ca84d6ca67954025
michel <sociumpatners@gmail.com>
2025-03-30 18:12:32 +0300

    Prices changed

--- src/Components/data.js
+++ src/Components/data.js
@@ -1,12 +1,14 @@
 const products = [
-  { id: 1, category: "TV", price: 1290, image: "/images/Cookers/cooker1.png" },
-  { id: 2, category: "Cooker", price: 999, image: "/images/Fridges/fridge2.png" },
-  { id: 3, category: "TV", price: 900, image: "/images/Fridges/fridge3.png" },
-  { id: 4, category: "Cooker", price: 1300, image: "/images/Fridges/fridge4.png" },
-  { id: 5, category: "TV", price: 1299, image: "/images/Fridges/fridge4.png" },
-  { id: 6, category: "Cooker", price: 2550, image: "/images/Fridges/fridge5.png" },
-  { id: 7, category: "TV", price: 799, image: "/images/Fridges/fridge6.png" },
-  { id: 8, category: "Cooker", price: 50, image: "/images/Fridges/fridge7.png" }
+  { id: 1, category: "TV", price: 200, image: "/images/Cookers/cooker1.png" },
+  { id: 2, category: "Cooker", price: 500, image: "/images/Fridges/fridge2.png" },
+  { id: 3, category: "TV", price: 250, image: "/images/Fridges/fridge3.png" },
+  { id: 4, category: "Cooker", price: 300, image: "/images/Fridges/fridge4.png" },
+  { id: 5, category: "TV", price: 400, image: "/images/Fridges/fridge4.png" },
+  { id: 6, category: "Cooker", price: 450, image: "/images/Fridges/fridge5.png" },
+  { id: 7, category: "TV", price: 100, image: "/images/Fridges/fridge6.png" },
+  { id: 8, category: "Cooker", price: 600, image: "/images/Fridges/fridge7.png" },
+  { id: 9, category: "TV", price: 1250, image: "/images/Cookers/cooker1.png" },
+  { id: 10, category: "Cooker", price: 600, image: "/images/Fridges/fridge2.png" },
 ];
 
 export default products;
