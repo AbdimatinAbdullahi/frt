6f0a50fe92fcf90e161a6184eaee03ca0818d9c2
michel <sociumpatners@gmail.com>
2025-04-01 10:44:01 +0300

    Names man

--- src/Components/data.js
+++ src/Components/data.js
@@ -1,14 +1,14 @@
 const products = [
   { id: 1, category: "TV", price: 200, image: "/images/Cookers/cooker1.png" },
-  { id: 2, category: "Cooker", price: 500, image: "/images/Fridges/fridge2.png" },
-  { id: 3, category: "TV", price: 250, image: "/images/Fridges/fridge3.png" },
-  { id: 4, category: "Cooker", price: 300, image: "/images/Fridges/fridge4.png" },
-  { id: 5, category: "TV", price: 400, image: "/images/Fridges/fridge4.png" },
-  { id: 6, category: "Cooker", price: 450, image: "/images/Fridges/fridge5.png" },
-  { id: 7, category: "TV", price: 100, image: "/images/Fridges/fridge6.png" },
-  { id: 8, category: "Cooker", price: 600, image: "/images/Fridges/fridge7.png" },
+  { id: 2, category: "Samsung Fridge AWT321", price: 500, image: "/images/Fridges/fridge2.png" },
+  { id: 3, category: "Fridge", price: 250, image: "/images/Fridges/fridge3.png" },
+  { id: 4, category: "Fridge", price: 300, image: "/images/Fridges/fridge4.png" },
+  { id: 5, category: "Fridge", price: 400, image: "/images/Fridges/fridge4.png" },
+  { id: 6, category: "Fridge", price: 450, image: "/images/Fridges/fridge5.png" },
+  { id: 7, category: "Fridge", price: 100, image: "/images/Fridges/fridge6.png" },
+  { id: 8, category: "Fridge", price: 600, image: "/images/Fridges/fridge7.png" },
   { id: 9, category: "TV", price: 1250, image: "/images/Cookers/cooker1.png" },
-  { id: 10, category: "Cooker", price: 600, image: "/images/Fridges/fridge2.png" },
+  { id: 10, category: "Fridge", price: 600, image: "/images/Fridges/fridge2.png" },
 ];
 
 export default products;
