7e6a04a394cf9627ab28420756fe8b6bd6bd21d6
michel <sociumpatners@gmail.com>
2025-04-01 10:47:15 +0300

    Names man again

--- src/Components/data.js
+++ src/Components/data.js
@@ -1,5 +1,5 @@
 const products = [
-  { id: 1, category: "TV", price: 200, image: "/images/Cookers/cooker1.png" },
+  { id: 1, category: "Gas Cooker", price: 200, image: "/images/Cookers/cooker1.png" },
   { id: 2, category: "Samsung Fridge AWT321", price: 500, image: "/images/Fridges/fridge2.png" },
   { id: 3, category: "Fridge", price: 250, image: "/images/Fridges/fridge3.png" },
   { id: 4, category: "Fridge", price: 300, image: "/images/Fridges/fridge4.png" },
@@ -7,7 +7,7 @@ const products = [
   { id: 6, category: "Fridge", price: 450, image: "/images/Fridges/fridge5.png" },
   { id: 7, category: "Fridge", price: 100, image: "/images/Fridges/fridge6.png" },
   { id: 8, category: "Fridge", price: 600, image: "/images/Fridges/fridge7.png" },
-  { id: 9, category: "TV", price: 1250, image: "/images/Cookers/cooker1.png" },
+  { id: 9, category: "Cooker", price: 1250, image: "/images/Cookers/cooker1.png" },
   { id: 10, category: "Fridge", price: 600, image: "/images/Fridges/fridge2.png" },
 ];
 
